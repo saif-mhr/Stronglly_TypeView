3f6c24170a7b2901207c594ddb12ea30886f2045
saif-mhr <saifullah786@hotmail.com>
2020-12-24 22:10:52 +0500

    Add project files.

---
+++ Stronglly_TypeView/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stronglly_TypeView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stronglly_TypeView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            Employee emp1 = new Employee();
+            emp1.name = "Saifullah";
+            emp1.gender = "male";
+            emp1.designation = "Lecturer";
+
+            Employee emp2 = new Employee();
+            emp1.name = "Shoaib mahar";
+            emp1.gender = "male";
+            emp1.designation = "Lecturer";
+
+            Employee emp3 = new Employee();
+            emp1.name = "Zulqarnain";
+            emp1.gender = "male";
+            emp1.designation = "Lecturer";
+
+            List<Employee> mylist = new List<Employee>();
+            mylist.Add(emp1);
+            mylist.Add(emp2);
+            mylist.Add(emp3);
+
+            return View(mylist);
+        }
+    }
+}
---
+++ Stronglly_TypeView/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stronglly_TypeView.Models
+{
+    public class Employee
+    {
+        public string name { get; set; }
+        public string gender{ get; set; }
+        public string designation { get; set; }
+    }
+}
